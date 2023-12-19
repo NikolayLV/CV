182bb529e0afa22b1286487f1ae32ed0dafab382
Nikolay <Nikolayd2002@gmail.com>
2023-12-19 14:37:40 +0100

    CV Create adaptive layout

--- app/views/type/creative.php
+++ app/views/type/creative.php
@@ -103,6 +103,65 @@
                     <img class="skills_img" src="<?php WWW ?>/assets/img/photoshop.svg" alt="">
                 </div>
             </div>
+
+            <div class="skills_img_mob">
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/php.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/js.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/html.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/react.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/github.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/vagrant.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/laravel.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/linux.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/apache.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/mysql.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/ts.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/css.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/nodejs.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/redux.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/bootstrap.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/figma.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/jetbrains.svg" alt="">
+                </div>
+                <div class="skills_div_img">
+                    <img class="skills_img" src="<?php WWW ?>/assets/img/photoshop.svg" alt="">
+                </div>
+            </div>
+
+
         </div>
         <div class="skills_right">
             <div class="skills_info">
